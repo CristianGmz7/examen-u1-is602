b1a1e8544a446e9e90ea6f2033d407f903a0c088
CristianGmz7 <cristiangomez.cagg78@gmail.com>
2025-02-17 09:16:25 -0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { globalStyles } from '@/styles/global-styles'
+import { Colors } from '@/constants/Colors'
+import { useFonts } from 'expo-font'
+
+
+    // const [  ] = useFonts({
+    //     NombreFuente: require('../assets/fonts/fuentedeGoogle.ttf'),
+    // })
+
+    /* Ejemplo de uso fontFamily: NombreFuente  */
+
+const App = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default App
---
+++ styles/global-styles.ts
@@ -0,0 +1,9 @@
+import { Colors } from "@/constants/Colors";
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // backgroundColor
+    },
+});
